4a6583468ea7306af58b15467d995feecaa68382
Karlp-Web <Karlenportillo1@gmail.com>
2024-11-01 11:06:01 -0700

    yomomma

--- Main.java
+++ Main.java
@@ -4,6 +4,18 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Rectangle r1 = new Rectangle(2,4);
+    Rectangle r2 = new Rectangle(4,2);
+
+    // Rotated if the length of 1 is equal to the width of 2 and vice versa
+    boolean isRotated = (r1.getLength() == r2.getWidth()) && (r1.getWidth() == r2.getLength());
+
+    //congruent if they either rotated, or exactly equal to each other; equals() from the rectrangle class will check
+    //equality of rectangles for me
+    boolean isCongruent = isRotated || r1.equals(r2);
+
+    //similar if they are either congruent, or if the ratio of the lengths is equal to the ration of the widths
+    boolean isSimilar = isCongruent || ((r1.getLength() / r2.getLength()) == (r1.getWidth() / r2.getWidth()));
+
   }
 }
